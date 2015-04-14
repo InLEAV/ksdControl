c7b5b30f6979c0cb898926336aa48c399ed86ccb
gamecmq <yin@yindeMacBook-Air.local>
2015-04-14 15:51:06 +0800

    add LoginView

---
+++ ksdControl/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ksdControl
+//
+//  Created by yin on 15-4-14.
+//  Copyright (c) 2015年 HANQING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
