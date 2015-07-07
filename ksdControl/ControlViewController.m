d90205b1db517755d4088b8f8370dd7f8e72afc4
gamecmq <gamecmq@gmail.com>
2015-07-07 12:26:18 +0800

    组合遍历例子 注释

--- ksdControl/ControlViewController.m
+++ ksdControl/ControlViewController.m
@@ -308,7 +308,11 @@ AppDelegate *appDelegate;
                                        forIndexPath:indexPath];
                 cell.label.lineBreakMode = NSLineBreakByTruncatingMiddle;
                 cell.label.text = ((GroupVO*)elementArray[rowNo]).aName;
-                
+//                for (int j =0; j < ((GroupVO*)(((AreaVO*)appDelegate.areaArray[areaIndex]).groups[i])).elements.count; j++)
+//                {
+//                    [array addObject:((GroupVO*)(((AreaVO*)appDelegate.areaArray[areaIndex]).groups[i])).elements[j]];
+//                }
+
                 return cell;
                 
             }
